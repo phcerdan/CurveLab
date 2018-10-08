48931f7a42ad98a80458a56017a01db0e7c2c85f
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2018-10-08 19:43:30 -0400

    Clang-format fdct3d/src

--- fdct3d/src/commoninc.hpp
+++ fdct3d/src/commoninc.hpp
@@ -1,39 +1,41 @@
 /* FDCT3D (Fast 3d Curvelet Transform)
    Copyright (C) 2004 Caltech
-	Written by Lexing Ying
+        Written by Lexing Ying
 */
 
 #ifndef _FDCT3DINC_HPP_
 #define _FDCT3DINC_HPP_
 
-//STL stuff
-#include <iostream>
-#include <fstream>
-#include <sstream>
+// STL stuff
 #include <string.h>
-#include <cfloat>
 #include <cassert>
+#include <cfloat>
 #include <cmath>
-#include <string>
 #include <complex>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
 
-#include <vector>
-#include <set>
-#include <map>
-#include <deque>
-#include <queue>
-#include <utility>
 #include <algorithm>
+#include <deque>
+#include <map>
+#include <queue>
+#include <set>
+#include <utility>
+#include <vector>
 using namespace std;
 
-//FFT stuff
+// FFT stuff
 #include "fftw.h"
 
-//typedef double double;
+// typedef double double;
 typedef complex<double> cpx;
 
-//AUX functions
-inline int pow2(int l) { assert(l>=0); return (1<<l); }
-
+// AUX functions
+inline int pow2(int l) {
+  assert(l >= 0);
+  return (1 << l);
+}
 
 #endif
