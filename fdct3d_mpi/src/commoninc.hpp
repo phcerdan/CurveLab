c43a40e1d6ee9b6f01fb3ef29c61e30933d34265
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2018-10-08 19:44:17 -0400

    Clang-format fdct3d

--- fdct3d_mpi/src/commoninc.hpp
+++ fdct3d_mpi/src/commoninc.hpp
@@ -1,43 +1,53 @@
 /* FDCT3D (Fast 3d Curvelet Transform)
    Copyright (C) 2004 Caltech
-	Written by Lexing Ying
+        Written by Lexing Ying
 */
 
 #ifndef _COMMONINC_HPP_
 #define _COMMONINC_HPP_
 
-//STL stuff
-#include <iostream>
+// STL stuff
 #include <fstream>
+#include <iostream>
 #include <sstream>
 
-#include <cfloat>
 #include <cassert>
+#include <cfloat>
 #include <cmath>
-#include <string>
 #include <complex>
+#include <string>
 
-#include <vector>
-#include <set>
-#include <map>
-#include <deque>
-#include <queue>
-#include <utility>
 #include <algorithm>
+#include <deque>
+#include <map>
+#include <queue>
+#include <set>
+#include <utility>
+#include <vector>
 using namespace std;
 
-//FFT stuff
+// FFT stuff
 #include "fftw.h"
 
-//typedef double double;
+// typedef double double;
 typedef complex<double> cpx;
 
-//AUX functions
-inline int pow2(int l) { assert(l>=0); return (1<<l); }
+// AUX functions
+inline int pow2(int l) {
+  assert(l >= 0);
+  return (1 << l);
+}
 
-//petsc stuff
+// petsc stuff
 #include "petscsnes.h"
-#define iC(fun)  { int ierr=fun; CHKERRQ(ierr); }
-#define iA(expr) { if(!(expr)) SETERRQ(1, "Assertion: "#expr" failed!"); }
+#define iC(fun)     \
+  {                 \
+    int ierr = fun; \
+    CHKERRQ(ierr);  \
+  }
+#define iA(expr)                                             \
+  {                                                          \
+    if (!(expr)) SETERRQ(1, "Assertion: " #expr " failed!"); \
+  }
 
 #endif
